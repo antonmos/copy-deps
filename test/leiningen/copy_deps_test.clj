3b4fb417454fd9dbcf443b5782fe05d63507a141
Ted Cushman <tcushman@gmail.com>
2019-04-26 08:55:38 -0500

    Oops, forgot to update the tests.

--- test/leiningen/copy_deps_test.clj
+++ test/leiningen/copy_deps_test.clj
@@ -1,54 +1,61 @@
 ;;;; Copyright © 2015-2017 José Pablo Fernández Silva
 
-(ns leiningen.jar-copier-test
+(ns leiningen.copy-deps-test
   (:require [clojure.test :refer :all]
-            [leiningen.jar-copier :refer :all]
+            [leiningen.copy-deps :refer :all]
             [clojure.java.io :as io]))
 
-(deftest no-jar-copier-config-issues-warning
+(deftest no-copy-deps-config-issues-warning
   (let [warnings (atom 0)]
     (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
-      (jar-copier {}))
+      (copy-deps {}))
     (is (= 1 @warnings))))
 
 (deftest no-destination-issues-warning
   (let [warnings (atom 0)]
     (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
-      (jar-copier {:jar-copier {}}))
+      (copy-deps {:copy-deps {}}))
     (is (= 1 @warnings))))
 
 (deftest nothing-to-copy-issues-warning
   (let [warnings (atom 0)]
     (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
-      (jar-copier {:jar-copier {:destination "target/jars"}}))
+      (copy-deps {:copy-deps {:destination "target/jars"}}))
     (is (= 1 @warnings))))
 
 (deftest lack-of-java-agents-issues-warning
   (let [warnings (atom 0)]
     (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
-      (jar-copier {:jar-copier {:java-agents true
-                                 :destination "target/jars"}}))
+      (copy-deps {:copy-deps {:java-agents true
+                              :destination "target/jars"}}))
     (is (= 1 @warnings))))
 
 (deftest copy-java-agent
-  (jar-copier {:java-agents '[[org.clojure/clojure "1.7.0"]]
-               :jar-copier  {:java-agents true
-                             :destination "target/jars"}})
-  (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar")))
-  (io/delete-file "target/jars/org.clojure/clojure.jar"))
-
-(deftest copy-plain-jar
-  (jar-copier {:jar-copier {:jars        '[[org.clojure/clojure "1.7.0"]]
+  (copy-deps {:java-agents '[[org.clojure/clojure "1.7.0"]]
+              :copy-deps   {:java-agents true
                             :destination "target/jars"}})
   (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar")))
   (io/delete-file "target/jars/org.clojure/clojure.jar"))
 
+(deftest copy-plain-jar
+  (copy-deps {:copy-deps {:dependencies '[[org.clojure/clojure "1.7.0"]]
+                          :destination  "target/jars"}})
+  (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar")))
+  (io/delete-file "target/jars/org.clojure/clojure.jar"))
+
 (deftest copy-plain-jar-and-java-agents
-  (jar-copier {:java-agents '[[org.clojure/clojure "1.7.0"]]
-               :jar-copier  {:jars        '[[org.clojure/clojure-contrib "1.2.0"]]
-                             :java-agents true
-                             :destination "target/jars"}})
+  (copy-deps {:java-agents '[[org.clojure/clojure "1.7.0"]]
+              :copy-deps   {:dependencies '[[org.clojure/clojure-contrib "1.2.0"]]
+                            :java-agents  true
+                            :destination  "target/jars"}})
   (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar")))
   (is (.exists (io/as-file "target/jars/org.clojure/clojure-contrib.jar")))
   (io/delete-file "target/jars/org.clojure/clojure.jar")
   (io/delete-file "target/jars/org.clojure/clojure-contrib.jar"))
+
+(deftest copy-plain-non-jar
+  (copy-deps {:copy-deps {:dependencies '[[org.clojure/clojure "1.7.0" :extension "pom"]]
+                          :destination  "target/misc"}})
+  (is (.exists (io/as-file "target/misc/org.clojure/clojure.pom")))
+  (io/delete-file "target/misc/org.clojure/clojure.pom"))
+
