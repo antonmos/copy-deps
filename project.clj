2deff3c67f8bf7f255533724043fc7ae4c5c7374
Ted Cushman <tcushman@opploans.com>
2021-03-23 10:04:43 -0500

    Version 0.8.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;;;; Copyright © 2015-2017 José Pablo Fernández Silva
 
-(defproject biiwide/copy-deps "0.7.0"
+(defproject biiwide/copy-deps "0.8.0-SNAPSHOT"
 
   :description "Copy files from your dependencies into your resources."
 
