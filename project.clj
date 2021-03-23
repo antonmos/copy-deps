ec166f8287a41a421c640ddcac941ce7bf09bca0
Ted Cushman <tcushman@opploans.com>
2021-03-23 13:46:34 -0500

    Updating releast-task configuration

--- project.clj
+++ project.clj
@@ -9,9 +9,16 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :lein-release {:deploy-via :clojars}
-
   :scm {:name "git"
         :url  "https://github.com/biiwide/copy-deps"}
 
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
