7542a6401ff2e69f243a299bf425b7cdde4c8609
Ted Cushman <tcushman@opploans.com>
2021-03-23 09:59:12 -0500

    Switching from copying to linking, and making verbose output optional.

--- src/leiningen/copy_deps.clj
+++ src/leiningen/copy_deps.clj
@@ -3,7 +3,8 @@
 (ns leiningen.copy-deps
   (:require [leiningen.core.main :as lein]
             [cemerick.pomegranate.aether :as aether]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import  (java.nio.file Files)))
 
 
 (defn parse-dependency
@@ -14,14 +15,44 @@
           :version  version}))
 
 
-(defn- do-copy-deps! [deps destination]
+(defn- readable-file? [x]
+  (try
+    (let [f (io/file x)]
+      (and (.exists f)
+           (.isFile f)
+           (.canRead f)))
+    (catch Exception _ false)))
+
+
+(defn- same-file? [file-a file-b]
+  (Files/isSameFile
+    (.toPath (io/file file-a))
+    (.toPath (io/file file-b))))
+
+
+(defn- link [src dst]
+  (Files/deleteIfExists
+    (.toPath (io/file dst)))
+  (Files/createLink
+    (.toPath (io/file dst))
+    (.toPath (io/file src))))
+
+
+(defn- do-copy-deps! [deps destination verbose?]
   (doseq [dep (keys (aether/resolve-dependencies :coordinates deps))]
-      (let [{:keys [artifact extension file]} (parse-dependency dep)
-            dst-file (io/file destination (str artifact "." (or extension "jar")))]
-        (when file
-          (lein/info "Copying" (.getPath file) "to" (.getPath dst-file))
-          (io/make-parents dst-file)
-          (io/copy file dst-file)))))
+    (let [{:keys [artifact extension file]} (parse-dependency dep)
+          dst-file (io/file destination (str artifact "." (or extension "jar")))]
+      (when (readable-file? file)
+        (io/make-parents dst-file)
+        (when verbose?
+          (lein/info "[copy-deps] Checking:" (.getPath dst-file)))
+        (if (and (.exists (io/file dst-file))
+                 (same-file? dst-file file))
+          (when verbose?
+            (lein/info "[copy-deps] Already Linked:" (.getPath dst-file) "to" (.getPath file) ))
+          (do (when verbose?
+                (lein/info "[copy-deps] Linking:" (.getPath dst-file) "to" (.getPath file)))
+              (link file dst-file)))))))
 
 
 (def ^{:private true} misses? (complement contains?))
@@ -36,10 +67,11 @@
       (if (and (misses? (:copy-deps project) :dependencies)
                (misses? (:copy-deps project) :java-agents))
         (lein/warn "copy-deps has nothing to copy, you need to specify :dependencies or :java-agents (or both).")
-        (let [destination (get-in project [:copy-deps :destination])]
+        (let [destination (get-in project [:copy-deps :destination])
+              verbose?    (get-in project [:copy-deps :verbose?] false)]
           (if (and (get-in project [:copy-deps :java-agents])
                    (misses? project :java-agents))
             (lein/warn "copy-deps requested to copy java agents, but no java agents were specified in your project.")
-            (do-copy-deps! (:java-agents project) destination))
+            (do-copy-deps! (:java-agents project) destination verbose?))
           (when (contains? (:copy-deps project) :dependencies)
-            (do-copy-deps! (get-in project [:copy-deps :dependencies]) destination)))))))
+            (do-copy-deps! (get-in project [:copy-deps :dependencies]) destination verbose?)))))))
